3a86943ca2cd9c44d3d0725504dcc91b32d9906e
Minho Cho <minhocho88@gmail.com>
2014-01-04 02:58:06 -0800

    ch.8 mod

--- chapter_6/table_of_contents.rb
+++ chapter_6/table_of_contents.rb
@@ -1,6 +1,10 @@
 line_width = 50
 puts("Table of Contents".center(line_width))
 puts ''
-puts("Chapter 1: Getting Started".ljust(line_width/2) + "page  1".rjust(line_width/2))
-puts("Chapter 2: Numbers        ".ljust(line_width/2) + "page  9".rjust(line_width/2))
-puts("Chapter 3: Letters        ".ljust(line_width/2) + "page 13".rjust(line_width/2))
+
+chapters = ["Chapter 1: Getting Started","Chapter 2: Numbers        ","Chapter 3: Letters        "]
+pages    = ["page  1","page  9","page 13"]
+
+3.times do |i|
+	puts ( chapters[i].ljust(line_width/2) + pages[i].rjust(line_width/2))
+end
